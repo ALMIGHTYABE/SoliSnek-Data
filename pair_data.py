7e756c43a47bfa0fd52bd330c2d8855a6a6d13ae
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2023-04-18 18:02:35 +0530

    Add files via upload

--- pair_data.py
+++ pair_data.py
@@ -72,7 +72,7 @@ try:
     pairdata_df["fee %"] = pairdata_df["type"]
     pairdata_df["fee %"].replace({"vsAMM": 0.20, "ssAMM": 0.02}, inplace=True)
 
-    edit_index_1 = pairdata_df[(pairdata_df["address"] == "0x5b9A790E602aDFA8c127c480a511AB9f90807595") or (pairdata_df["address"] == "0x8d979AF11481E0BddfBB0cA7D6D8E20Ec6855f00") or (pairdata_df["address"] == "0xB46a84fa208F71cF598a1b50fdb5Bec3213B5845")].index
+    edit_index_1 = pairdata_df[(pairdata_df["address"] == "0x5b9A790E602aDFA8c127c480a511AB9f90807595") | (pairdata_df["address"] == "0x8d979AF11481E0BddfBB0cA7D6D8E20Ec6855f00") | (pairdata_df["address"] == "0xB46a84fa208F71cF598a1b50fdb5Bec3213B5845")].index
     pairdata_df.loc[edit_index_1, "fee %"] = 2
 
     pairdata_df["dailyVolumeUSD"] = pd.to_numeric(pairdata_df["dailyVolumeUSD"])
